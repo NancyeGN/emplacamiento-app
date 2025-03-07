5c053cfb1cebd4800532dd0bc8e45e9dc20bc6b4
Nancy González <nancy.gonzalez@g-tech.io>
2025-03-07 12:28:21 -0600

    se corrigen errores

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { use } from "react";
-import { Metadata } from "next";
-import { ClerkProvider,
-         SignInButton,
-         SignUpButton,
-         SignedIn,
-         SignedOut,
-         UserButton
- } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Header } from "../components/Header";
-import { Inter } from "next/font/google"
 import "./globals.css"
 
 
